1a4f27dcc0075b2e5e67130ba5fb08e7a86ef2bd
tlebel <thom.lebel@orange.fr>
2019-10-16 18:54:50 +0200

    Ending animation

---
+++ MattLife/Assets/Scripts/PlayerEnd.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEnd : MonoBehaviour
+{
+	public void PlayWalkSound()
+	{
+		AudioManager.instance.PlaySound("PlayerWalk");
+	}
+}
